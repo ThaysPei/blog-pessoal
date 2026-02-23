9d85311a912e8585948657f98fcf5c67aa3e3fc5
Thays Peixoto <thabysilva12@gmail.com>
2026-02-23 15:53:17 -0300

    atualiza importações route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import FormTema from './components/tema/formtema/FormTema'
-import ListaTemas from './components/tema/listatemas/ListaTemas'
-import { AuthProvider } from './context/AuthContext'
-import Cadastro from './pages/cadastro/Cadastro'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import DeletarTema from './components/tema/deletartema/DeletarTema'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import FormTema from "./components/tema/formtema/FormTema";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import { AuthProvider } from "./context/AuthContext";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
+import ListaPostagens from "./components/postagem/listapostagem/ListaPostagens";
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 
 function App() {
   return (
@@ -24,13 +27,17 @@ function App() {
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
               <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
