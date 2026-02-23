cdd5eba09a4d3483b8241f151538cb0327184b98
Thays Peixoto <thabysilva12@gmail.com>
2026-02-23 16:16:08 -0300

    adiciona rota de login

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
           <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/temas" element={<ListaTemas />} />
